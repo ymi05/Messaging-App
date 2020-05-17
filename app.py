bb351476bf4d13fa5948fb9fcc1f1c7644b42bf8
Youssef Itani <44197563+ymi05@users.noreply.github.com>
2020-05-17 17:02:05 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from tkinter import scrolledtext
 import socket
+import threading
+import time
 
 class MessageApp():
     def __init__(self,master):
+      
         self.master = master
         self.master.title("Message App")
         self.master.geometry('405x230')
@@ -26,44 +29,47 @@ class MessageApp():
         self.FORMAT = "utf-8"
         self.DISCONNECT_MESSAGE = "!DISCONNECT" 
 
-        self.SERVER = "" #the IP address of the server goes here
+        self.SERVER = "192.168.1.36"
         self.ADDR = (self.SERVER,self.PORT)
 
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
         self.client.connect(self.ADDR)
-        # msg = ""
-        # while msg!="!DISCONNECT":
-        #     msg = self.messageEntry.get()
-
-          
-
+        self.thread = threading.Thread(target=self.waitForResponse)
+        self.thread.start()
+   
 
 
     def sendMessage(self,message):
         self.txtField.insert(INSERT, message)
-        # self.waitForResponse()
+
+     
+
 
 
     def send(self,msg):
         self.messageEntry.delete(0, END)
         self.messageEntry.insert(0, "")
-        self.sendMessage(msg+'\n')
+
         message = msg.encode(self.FORMAT)
         msg_length = len(message)
         send_length = str(msg_length).encode(self.FORMAT)
         send_length += b' ' * (self.HEADER - len(send_length))#the byte rep of this string
         self.client.send(send_length)
         self.client.send(message)
-     
+        if(self.DISCONNECT_MESSAGE == message.decode(self.FORMAT)):
+            sys.exit()
+            self.master.destroy()
+        
     
-    # def waitForResponse(self):
-    #     while True:
-    #         serverMessage = self.client.recv(1024)
-    #         if len(serverMessage) <= 0:
-    #             break
-    #         serverMessage = serverMessage.decode(self.FORMAT)
-    #         self.sendMessage(serverMessage)
+    def waitForResponse(self):
+        while True:
+            serverMessage = self.client.recv(1024)
+        
+            if len(serverMessage) <= 0:
+                break
+            serverMessage = serverMessage.decode(self.FORMAT)
+            self.sendMessage(serverMessage)
 def main():
 
     window = Tk()
